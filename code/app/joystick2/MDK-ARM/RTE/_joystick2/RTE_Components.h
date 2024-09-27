0c4cd6283211550a42412c281ab0b00087782044
chenzhjie <jaychen@m5stack.com>
2024-09-27 09:40:22 +0800

    style: change project name

--- code/app/joystick2/MDK-ARM/RTE/_joystick2/RTE_Components.h
+++ code/app/joystick2/MDK-ARM/RTE/_joystick2/RTE_Components.h
@@ -3,8 +3,8 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'joystice2' 
- * Target:  'joystice2' 
+ * Project: 'joystick2' 
+ * Target:  'joystick2' 
  */
 
 #ifndef RTE_COMPONENTS_H
